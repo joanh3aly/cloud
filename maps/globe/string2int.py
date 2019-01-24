da39e6b48e41015de682dadd588a6fa3e5a4ae33
Joan Healy <joan@altv.com>
2019-01-24 13:24:20 +0000

    new csv

--- maps/globe/string2int.py
+++ maps/globe/string2int.py
@@ -3,10 +3,11 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.read_csv('https://raw.githubusercontent.com/joanh3aly/cloud/master/maps/globe/worldcities.csv')
+df = pd.read_csv('/Applications/MAMP/htdocs/cloud/maps/globe/worldcities-2.csv')
 
 
 #print(df)
+
 
 lat = df.iloc[:,2]
 lng = df.iloc[:,3]
@@ -14,10 +15,8 @@ city = df["city"]
 
 #df[["lat", "lng"]] = df[["lat", "lng"]].apply(pd.to_numeric)
 
-#print(df.dtypes)
-
 df = pd.DataFrame({'city': city, 'lat': lat, 'lng': lng})
 print(df.dtypes)
 #print(df)
 
-df.to_csv("cleanedCities-1.csv", sep=',', encoding='utf-8')
+df.to_csv("cleanedCities-5.csv", sep=',', encoding='utf-8')
