9356be067e0c0fc5ebbbe65c82a6530912890532
Joan Healy <joan@altv.com>
2019-01-24 12:35:15 +0000

    cleaned up csv data

---
+++ maps/globe/string2int.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/joanh3aly/cloud/master/maps/globe/worldcities.csv')
+
+
+#print(df)
+
+lat = df.iloc[:,2]
+lng = df.iloc[:,3]
+city = df["city"]
+
+#df[["lat", "lng"]] = df[["lat", "lng"]].apply(pd.to_numeric)
+
+#print(df.dtypes)
+
+df = pd.DataFrame({'city': city, 'lat': lat, 'lng': lng})
+print(df.dtypes)
+#print(df)
+
+df.to_csv("cleanedCities.csv", sep='\t', encoding='utf-8')
