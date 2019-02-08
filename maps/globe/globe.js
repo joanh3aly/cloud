088af361593b95266fd09b61d337fc9ddfd7489a
Joan Healy <joan@altv.com>
2019-02-08 15:23:50 +0000

    update JSON file/added setInterval function

--- maps/globe/globe.js
+++ maps/globe/globe.js
@@ -20,19 +20,18 @@ function ready(error,data,capitalsData) {
 
   var countries = topojson.feature(data, data.objects.countries).features;
   //console.log(JSON.stringify(countries, null, 2));
-
   //var capitals = json.feature(data, capitalsData.objects.countries).features;
-  //var capitals = json.feature(data, capitalsData.objects.countries).features;
-
+ 
+  /*
+    Convert json data to JS array
+  */
   var capitals = [];
-
   for(var i in capitalsData) {
     capitals.push([capitalsData[i]]);
     //console.log(i);
   }
   console.log(capitals);
-
-  //console.log(JSON.stringify(capitalsData, null, 2));
+  
 
   var projection = d3.geoMercator()
     .scale(100)
@@ -40,19 +39,6 @@ function ready(error,data,capitalsData) {
 
   var path = d3.geoPath()
     .projection(projection);
-
-/*    
-  var capitalsArray = []  
-  for ( var i = 1; i <= capitals.length; ++i) {
-     //console.log(capitals[i].lat)  
-     if (capitals[i] == 1) {
-    //   console.log(capitals[i].lat);
-     }
-     capitalsArray += capitals[i];
-     
-  }  
-  console.log(capitalsArray[5]);
-*/
 
   svg
     .selectAll(".country")
@@ -70,67 +56,84 @@ function ready(error,data,capitalsData) {
     });
     
     //var timer = d3.timer(callback);
-    /*
-      we just need to display the timestamp?
-    */
-    svg
-      .append("g") 
-      .attr("class", "city-group")
-      .selectAll(".city-circle")
-      .data(capitals)
-      //console.log(capitals.lat)
-      .enter()
-      .append("circle")
-      .style("fill", function(d) {            
-        if (d.alert == 1) {
-          return "red"
-        } else { 
-            return "black" 
-          }          
-      ;}) 
-      .attr("r", function(d) {            
-        if (d.alert == 1) {
-          return 6
-        } else { 
-            return 2
-          }          
-      ;})  
-    /*  
-      .transition()
-      .ease(d3.easeLinear)
-      .duration(2000)   
-      */               
-     // .attr("class", "city-circle")
-      .attr("cx",10)
-      .attr("cy",10)
-       
-      .attr("cx",function(d){
-        var coords = projection([d.lat, d.lng])
-        return coords[0];
-      })
+    
+    
+
+    var interval = setInterval(function () {
+      //add your new fresh data here
+      update();
+    }, 5000);
+
+    function update () {
+    var hourlyCapitals = [];  
+    for (var j = 0; j < capitals.length; ++j) {
+      for (var k = 1; k <= 24; ++k) {
+        if (capitals[j][0].timestamp == k && capitals[j][0].alert == 1) {
+          hourlyCapitals.push([capitals[j][0]]);
+        }}}
+        console.log(hourlyCapitals[0][0]);
+        console.log(hourlyCapitals[1][0]);
+        console.log(hourlyCapitals[2][0]);
+   //     for (var l = 0; l < hourlyCapitals.length; ++l) {
+        //  console.log(capitals[j][0].alert);
+          svg
+            .append("g") 
+            .attr("class", "city-group")
+            .selectAll(".city-circle")
+            .data(hourlyCapitals)
+            .enter()
+            .append("circle")
+            .style("fill", function(d) {   
+             // console.log(d);         
+              if (d[0].alert == 1) {
+                return "red"
+              } else { 
+                  return "black" 
+                }          
+            ;}) 
+            .attr("r", function(d) {            
+              if (d[0].alert == 1) {
+                return 6
+              } else { 
+                  return 2
+                }          
+            ;})                 
+          // .attr("class", "city-circle")
+            .attr("cx",10)
+            .attr("cy",10)
+            .attr("cx",function(d){
+              var coords = projection([d[0].lat, d[0].lng])
+              return coords[0];
+            })
+            .attr("cy",function(d){  
+              var coords = projection([d[0].lat, d[0].lng])
+              //console.log(d[0].lat);
+              return coords[1];
+            }) 
+
+            svg.selectAll(".city-label")
+              .data(capitals)
+              .enter()
+              .append("text")
+              .attr("class","city-label")
+              .attr("x",function(d){
+                var coords = projection([d[0].lat, d[0].lng])
+                return coords[0];
+              })
+              .attr("y",function(d){  
+                var coords = projection([d[0].lat, d[0].lng])
+                return coords[1];
+              }) 
+              .text(function(d){
+                return d[0].city
+              })
+              .attr("dx",2)
+              .attr("dy",2)  
+      //    }
+     //   }
+     // }
+    }
+
       
-      .attr("cy",function(d){  
-        var coords = projection([d.lat, d.lng])
-        return coords[1];
-      }) 
-     
-      svg.selectAll(".city-label")
-        .data(capitals)
-        .enter()
-        .append("text")
-        .attr("class","city-label")
-        .attr("x",function(d){
-          var coords = projection([d.lat, d.lng])
-          return coords[0];
-        })
-        .attr("y",function(d){  
-          var coords = projection([d.lat, d.lng])
-          return coords[1];
-        }) 
-        .text(function(d){
-          return d.city
-        })
-        .attr("dx",2)
-        .attr("dy",2)  
     
 };
